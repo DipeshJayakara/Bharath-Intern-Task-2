8c1cec22dbac7c29dea1aeda1538eecad351acfd
DipeshJayakara <142597315+DipeshJayakara@users.noreply.github.com>
2023-11-17 09:50:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const apiKey= "1e59b0c45d7ffa9cd1738963ac8d8189";
+        const apiUrl="https://api.openweathermap.org/data/2.5/weather?&unit=metric&q=";
+        const searchBox = document.querySelector(".search input");
+        const searchBtn = document.querySelector(".search button");
+
+        async function checkWeather(city){
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            var data = await response.json();
+
+            console.log(data);
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML =     Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        }
+        searchBtn.addEventListener("click",()=>{
+            checkWeather(searchBox.value);
+        })
